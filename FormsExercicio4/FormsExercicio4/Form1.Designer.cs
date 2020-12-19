92e3fc85da633cc262064876c46ed57f77a2c1d5
DaniloSant03 <75862223+DaniloSant03@users.noreply.github.com>
2020-12-19 14:07:21 -0300

    Add files via upload

---
+++ FormsExercicio4/FormsExercicio4/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FormsExercicio4
+{
+    partial class frmCalculadoraDeAreaDeTriangulo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblAreaValor = new System.Windows.Forms.Label();
+            this.btnCalcularArea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(130, 32);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(194, 29);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "área de triângulo";
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBase.Location = new System.Drawing.Point(56, 90);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(46, 20);
+            this.lblBase.TabIndex = 1;
+            this.lblBase.Text = "Base";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(56, 119);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(51, 20);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura";
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(135, 90);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(175, 20);
+            this.txtBase.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(135, 116);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(175, 20);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(56, 163);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(56, 20);
+            this.lblArea.TabIndex = 5;
+            this.lblArea.Text = "Área =";
+            // 
+            // lblAreaValor
+            // 
+            this.lblAreaValor.AutoSize = true;
+            this.lblAreaValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAreaValor.Location = new System.Drawing.Point(188, 163);
+            this.lblAreaValor.Name = "lblAreaValor";
+            this.lblAreaValor.Size = new System.Drawing.Size(51, 20);
+            this.lblAreaValor.TabIndex = 6;
+            this.lblAreaValor.Text = "label5";
+            // 
+            // btnCalcularArea
+            // 
+            this.btnCalcularArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularArea.Location = new System.Drawing.Point(163, 226);
+            this.btnCalcularArea.Name = "btnCalcularArea";
+            this.btnCalcularArea.Size = new System.Drawing.Size(92, 33);
+            this.btnCalcularArea.TabIndex = 7;
+            this.btnCalcularArea.Text = "Calcular";
+            this.btnCalcularArea.UseVisualStyleBackColor = true;
+            this.btnCalcularArea.Click += new System.EventHandler(this.clickou_calcular);
+            // 
+            // frmCalculadoraDeAreaDeTriangulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 304);
+            this.Controls.Add(this.btnCalcularArea);
+            this.Controls.Add(this.lblAreaValor);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmCalculadoraDeAreaDeTriangulo";
+            this.Text = "Calculadora área do Triângulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblAreaValor;
+        private System.Windows.Forms.Button btnCalcularArea;
+    }
+}
+
